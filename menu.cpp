0795256b9c4439d157847326b449e5be2956c96b
Artur Kunicki <tektmap1@gmail.com>
2023-11-15 17:23:36 +0100

    rozbudowa menu

--- menu.cpp
+++ menu.cpp
@@ -4,6 +4,7 @@
 #include <QFuture>
 #include <QtConcurrent>
 
+
 QSettings settings("bank_admin", "bank_admin");
 
 Menu::Menu(QWidget *parent) :
@@ -18,9 +19,25 @@ Menu::Menu(QWidget *parent) :
     QJsonDocument response = api_service->get("http://127.0.0.1:8000/adminpanel/user_count");
     QJsonObject jobj = response.object();
 
+    QJsonDocument response_1 = api_service->get("http://127.0.0.1:8000/adminpanel/employe_count");
+    QJsonObject jobj_1 = response_1.object();
+
+    QJsonDocument response_2 = api_service->get("http://127.0.0.1:8000/adminpanel/ticket_count");
+    QJsonObject jobj_2 = response_2.object();
+
+
     int value = jobj["value"].toInt();
-    std::string t = "ilosc pracownikow: " + std::to_string(value);
+    std::string t = "ilosc uzytkownikow: " + std::to_string(value);
+
+    int value1 = jobj_1["value"].toInt();
+    std::string t1 = "ilosc pracownikow: " + std::to_string(value1);
+
+    int value2 = jobj_2["value"].toInt();
+    std::string t2 = "ilosc wnioskow: " + std::to_string(value2);
+
     ui->label->setText(QString::fromStdString(t));
+    ui->label_2->setText(QString::fromStdString(t1));
+    ui->label_3->setText(QString::fromStdString(t2));
 
 
 
@@ -61,13 +78,14 @@ void Menu::funkcja2()
 // Panel pracowniczy
 void Menu::on_pushButton_2_clicked()
 {
-    // Implementuj logikę dla panelu pracowniczego
+    // Implementuj logikę dla panelu pracowniczego nowy widok
+
 }
 
 // Wnioski
 void Menu::on_pushButton_3_clicked()
 {
-    // Implementuj logikę dla panelu wniosków
+   //nowy widok i powrot do menu
 }
 
 // Wyloguj
